949d07d6a9ae03f2efcfaf43210b88de665ff25f
Lanzer-Lee <879988920@qq.com>
2024-03-21 00:03:16 +0800

    5th commit

--- Control/control.c
+++ Control/control.c
@@ -1,5 +1,39 @@
 #include "control.h"
 
+PID_TypeDef g_speed_pid;
+
+void pid_init(void){
+    g_speed_pid.SetPoint=0;
+    g_speed_pid.ActualValue=0.0;
+    g_speed_pid.SumError=0.0;
+    g_speed_pid.Error=0.0;
+    g_speed_pid.LastError=0.0;
+    g_speed_pid.PrevError=0.0;
+    g_speed_pid.Proportion=KP;
+    g_speed_pid.Integral=KI;
+    g_speed_pid.Derivative=KD;
+}
+
+int16_t increment_pid_control(PID_TypeDef *PID,float feedback_value){
+    PID->Error=(float)(PID->SetPoint-feedback_value);                                                     
+    PID->ActualValue += (PID->Proportion * (PID->Error - PID->LastError))                          
+                        + (PID->Integral * PID->Error)                                             
+                        + (PID->Derivative * (PID->Error - 2 * PID->LastError + PID->PrevError)); 
+    PID->PrevError = PID->LastError;                                       
+    PID->LastError = PID->Error;
+    return ((int16_t)(PID->ActualValue));
+}
+
+int16_t position_pid_control(PID_TypeDef *PID,float feedback_value){
+    PID->Error=(float)(PID->SetPoint-feedback_value);
+    PID->SumError += PID->Error;
+    PID->ActualValue = (PID->Proportion * PID->Error)                     
+                       + (PID->Integral * PID->SumError)                    
+                       + (PID->Derivative * (PID->Error - PID->LastError)); 
+    PID->LastError = PID->Error;
+    return ((int16_t)(PID->ActualValue));
+}
+
 uint8_t aim_ball_mid(void){
     if(0<=target_x && target_x<157){
         standard_left(100,100);
--- Control/control.h
+++ Control/control.h
@@ -6,5 +6,38 @@
 
 #define NOTMID  0
 #define MID     1
+#define INCR_LOCT_SELECT    1
+
+#if INCR_LOCT_SELECT
+/*increment PID*/
+#define KP  8.50f
+#define KI  5.00f
+#define KD  0.10f
+#define SMAPLSE_PID_SPEED   50
+#else
+/*position PID*/
+#define KP  10.0f
+#define KI  6.00f
+#define KD  0.50f
+#define SMAPLSE_PID_SPEED   50
+#endif
+
+typedef struct {
+    __IO float SetPoint;
+    __IO float ActualValue;
+    __IO float SumError;
+    __IO float Proportion;
+    __IO float Integral;
+    __IO float Derivative;
+    __IO float Error;
+    __IO float LastError;
+    __IO float PrevError;
+}PID_TypeDef;
+
+extern PID_TypeDef g_speed_pid;
+
+void pid_init(void);
+int16_t increment_pid_control(PID_TypeDef *PID,float feedback_value);
+int16_t position_pid_control(PID_TypeDef *PID,float feedback_value);
 
 #endif
